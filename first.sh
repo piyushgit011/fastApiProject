537620ac245ad1c272d5676a3b61ae97ee477538
Piyush Aaryan <piyushaaryan011@gmail.com>
2024-04-04 18:05:46 +0530

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,5 +1,6 @@
-apt-get update 
-apt-get install python
-apt-get install tmux 
+sudo apt-get update 
+sudo apt-get install python
+sudo apt-get install tmux 
+sudo apt-get install ffmpeg
 tmux new
 
