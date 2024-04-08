04b1c88f08ca57801fb0716ba4afffc3b4498bac
Piyush Aaryan <piyushaaryan011@gmail.com>
2024-04-08 22:02:58 +0530

    Update first.sh

--- first.sh
+++ first.sh
@@ -2,5 +2,7 @@ sudo apt-get update
 sudo apt-get install pip
 sudo apt-get install tmux 
 sudo apt-get install ffmpeg
+sudo apt-get install imagemagick
+cat /etc/ImageMagick-6/policy.xml | sed 's/none/read,write/g'> /etc/ImageMagick-6/policy.xml
 tmux new
 
